3ae68b042712382eb77595bc172cfe119b4812b8
NillaCdrbrg <nillacdrbrg@gmail.com>
2026-05-20 20:27:04 +0200

    Experimentation

---
+++ scripts/game.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var tile_map_layer: TileMapLayer = $Tilemap/TileMapLayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var mousePos: Vector2 = get_global_mouse_position()
+	var localToMap: Vector2 = tile_map_layer.local_to_map(mousePos)
+	#print("localToMap: " + str(localToMap))
+	#var atlasCoords: Vector2 = tile_map_layer.get_cell_atlas_coords(localToMap)
+	#print("atlasCoords: " + str(atlasCoords)) #It returned where in the sprite-sheet it is.
+	var tileData: TileData = tile_map_layer.get_cell_tile_data(localToMap)
+	#print("tileData: " + str(tileData))
+	if tileData != null:
+		pass
+		#print("tileData.terrain: " + str(tileData.terrain))
